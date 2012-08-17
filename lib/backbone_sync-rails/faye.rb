5adff24977e5b174a555c31ac2c5f7852791c5eb
Max Gillett <max.gillett@gmail.com>
2012-08-17 15:30:28 -0300

    Update lib/backbone_sync-rails/faye.rb

--- lib/backbone_sync-rails/faye.rb
+++ lib/backbone_sync-rails/faye.rb
@@ -4,7 +4,7 @@ module BackboneSync
   module Rails
     module Faye
       mattr_accessor :root_address
-      self.root_address = 'http://localhost:9292'
+      self.root_address = 'http://glacial-oasis-6992.herokuapp.com'
     end
   end
 end
