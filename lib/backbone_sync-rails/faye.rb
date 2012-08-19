109c65294721033bc6b58a7a4edf29c716575539
Max Gillett <max.gillett@gmail.com>
2012-08-19 17:22:59 -0400

    changed root_address to localhost

--- lib/backbone_sync-rails/faye.rb
+++ lib/backbone_sync-rails/faye.rb
@@ -4,7 +4,7 @@ module BackboneSync
   module Rails
     module Faye
       mattr_accessor :root_address
-      self.root_address = 'http://glacial-oasis-6992.herokuapp.com'
+      self.root_address = 'http://localhost:3000'
     end
   end
 end
